c6d53b0163c191bc409ee9b6947265a5681474f9
David Kessler <39646674+pairing4good@users.noreply.github.com>
2022-11-10 20:38:02 +0000

    updated tests and readme

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# 004 Variable Types
+# 005 Strings
 
-[![004 Variable Types](https://img.youtube.com/vi/a7rjSr7mZEA/0.jpg)](https://www.youtube.com/watch?v=a7rjSr7mZEA)
+[![005 Strings](https://img.youtube.com/vi/hnfHtsQITqM/0.jpg)](https://www.youtube.com/watch?v=hnfHtsQITqM)
 
 the index.js file
-- should define a string variable named animalName
-- should define a numeric variable named height
-- should define a boolean variable named isIndoorPet
-- should assign the innerHTML of the HTML element with the id result to the animalName
+- should define a string variable named `firstName`
+- should define a string variable named `jobTitle`
+- should define a string variable named `city`
+- should define a string template named `newJobPost` that contains `firstName`
+- should define a string template named `newJobPost` that contains `jobTitle` 
+- should define a string template named `newJobPost` that contains `city`
+- should assign the innerHTML of the HTML element with the id `result` to the `newJobPost`
 
 resources:
 - https://www.w3schools.com/js/js_let.asp
+- https://www.w3schools.com/js/js_string_templates.asp
 - https://www.w3schools.com/jsref/met_document_getelementbyid.aspd
 - https://www.w3schools.com/jsref/prop_html_innerhtml.asp
--- test/verify.test.js
+++ test/verify.test.js
@@ -37,31 +37,45 @@ afterEach(async () => {
   await browser.close();
 });
 
-describe('the index.js file', () => {
-  it('should define a string variable named animalName', async () => {
-    const animalName = await page.evaluate(() => animalName);
-    expect(typeof animalName).toBe('string');
+describe("the index.js file", () => {
+  it("should define a string variable named firstName", async () => {
+    const firstName = await page.evaluate(() => firstName);
+    expect(firstName).toBeDefined();
   });
-  
-  
-  it('should define a numeric variable named height', async () => {
-    const height = await page.evaluate(() => height);
-    expect(typeof height).toBe('number');
+
+  it("should define a string variable named jobTitle", async () => {
+    const jobTitle = await page.evaluate(() => jobTitle);
+    expect(jobTitle).toBeDefined();
   });
-  
-  
-  it('should define a boolean variable named isIndoorPet', async () => {
-    const isIndoorPet = await page.evaluate(() => isIndoorPet);
-    expect(typeof isIndoorPet).toBe('boolean');
+
+  it("should define a string variable named city", async () => {
+    const city = await page.evaluate(() => city);
+    expect(city).toBeDefined();
   });
-  
-  
-  it('should assign the innerHTML of the HTML element with the id result to the animalName', async () => {
-    const animalName = await page.evaluate(() => animalName);
-    const innerHtml = await page.$eval('#result', (result) => {
+
+  it("should define a string template named newJobPost that contains firstName", async () => {
+    const newJobPost = await page.evaluate(() => newJobPost);
+    const firstName = await page.evaluate(() => firstName);
+    expect(newJobPost).toContain(firstName);
+  });
+
+  it("should define a string template named newJobPost that contains jobTitle", async () => {
+    const newJobPost = await page.evaluate(() => newJobPost);
+    const jobTitle = await page.evaluate(() => jobTitle);
+    expect(newJobPost).toContain(jobTitle);
+  });
+
+  it("should define a string template named newJobPost that contains city", async () => {
+    const newJobPost = await page.evaluate(() => newJobPost);
+    const city = await page.evaluate(() => city);
+    expect(newJobPost).toContain(city);
+  });
+
+  it("should assign the innerHTML of the HTML element with the id result to the newJobPost", async () => {
+    const newJobPost = await page.evaluate(() => newJobPost);
+    const innerHtml = await page.$eval("#result", (result) => {
       return result.innerHTML.trim();
     });
-    
-    expect(innerHtml).toBe(animalName);
+    expect(innerHtml).toBe(newJobPost);
   });
 });
